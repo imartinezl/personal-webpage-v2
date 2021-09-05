fe3b3127fec630e21fcc85b2c17d6431ad6063c7
imartinezl <inigomlap@gmail.com>
2021-09-05 22:45:41 +0200

    Tailwind: custom transition and saturation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
+            transitionTimingFunction: {
+                'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+                'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+                'custom': 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+            },
+            saturate: {
+                25: '.25',
+                75: '.75',
+            }
         },
     },
     variants: {
@@ -17,6 +26,8 @@ module.exports = {
             borderOpacity: ['hover', 'group-hover'],
             fontWeight: ['hover', 'group-hover'],
             display: ['last'],
+            translate: ['hover', 'group-hover'],
+            saturate: ['hover', 'group-hover'],
         },
     },
     plugins: [
