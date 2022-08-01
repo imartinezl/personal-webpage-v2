244d7d3f2e0dd94560333f37afb2a7488fd17096
imartinezl <inigomlap@gmail.com>
2022-08-01 18:32:19 +0200

    Letter spacing for hover bold effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        letterSpacing: {
+            '1': '-0.014em'
+        },
         extend: {
             transitionTimingFunction: {
                 'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
@@ -23,11 +26,14 @@ module.exports = {
     variants: {
         extend: {
             borderWidth: ['hover', 'group-hover'],
+            letterSpacing: ['hover', 'group-hover'],
             borderOpacity: ['hover', 'group-hover'],
             fontWeight: ['hover', 'group-hover'],
             display: ['last'],
             translate: ['hover', 'group-hover'],
             saturate: ['hover', 'group-hover'],
+            scale: ['hover', 'group-hover', 'active', 'focus'],
+            cursor: ['hover', 'group-hover'],
         },
     },
     plugins: [
